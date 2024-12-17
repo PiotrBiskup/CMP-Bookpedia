6502f730172eb4162e8eeb0dee5f8778328ced20
piotr.biskup <piotr.biskup4@gmail.com>
2024-12-17 11:26:03 +0100

    presentation layer

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
+import com.plcoding.bookpedia.book.presentation.book_list.books
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Composable
@@ -29,4 +32,13 @@ fun BookSearchBarPreview2() {
             .background(Color.White)
     )
     BookSearchBar("Kotlin", {}, {}, modifier = Modifier.fillMaxWidth())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(searchResults = books),
+        onAction = {}
+    )
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,9 +5,25 @@ import com.plcoding.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = emptyList(),
+    val searchResults: List<Book> = books,
     val favouriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
-    val errorText: UiText? = null
+    val errorMessage: UiText? = null
 )
+
+val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Me, Myself and I"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.45633,
+        ratingCount = 5,
+        numPages = 230,
+        numEditions = 3
+    )
+}
